ecdaabadf85f7fd98566ad3e330ab633670925a9
Diego Salcovsky <dsalcovs@Diegos-MacBook-Pro.local>
2023-05-14 09:40:04 -0400

    add_get_firmware_updates_by_device_id

--- app.py
+++ app.py
@@ -43,4 +43,31 @@ def upload_firmware_update():
 
 @app.route('/get_firmware_updates_by_device_id', methods=['GET'])
 def get_firmware_updates_by_device_id():
+    email = request.args.get('email')
+    device_id = request.args.get('device_id')
+    secret = request.args.get('secret')
 
+    # validation
+    is_user_allow_to_access_device_updates = val_service.is_user_allow_to_access_device_updates(email,
+                                                                                                device_id,
+                                                                                                secret)
+    if not is_user_allow_to_access_device_updates:
+        return "Invalid request"
+
+    firmware_update_history = session.query(models.DeviceFirmwareUpdates).filter(
+        models.DeviceFirmwareUpdates.device_id == device_id
+    ).order_by(
+        models.DeviceFirmwareUpdates.date.desc()
+    ).all()
+
+    updates = []
+    for update in firmware_update_history:
+        json_update = {
+            "id": update.id,
+            "device_id": update.device_id,
+            "version": update.version,
+            "date": update.date
+        }
+        updates.append(json_update)
+
+    return updates
--- services/validation_service.py
+++ services/validation_service.py
@@ -10,13 +10,26 @@ class ValidationService:
         self.session = session
 
     def validate_device(self, device_id, secret):
-        device_api_key = self.session.query(models.DeviceApiKeys).filter(models.DeviceApiKeys.device_id == device_id,
-                                                                         models.DeviceApiKeys.secret == secret).count()
+        device_api_key_count = self.session.query(models.DeviceApiKeys).filter(
+            models.DeviceApiKeys.device_id == device_id,
+            models.DeviceApiKeys.secret == secret
+        ).count()
 
-        return True if device_api_key else False
+        return True if device_api_key_count else False
 
-    def validate_user(self, device_id, email):
-        pass
+    def is_user_allow_to_access_device_updates(self, email, device_id, secret):
+        device = self.session.query(models.Devices).filter(models.Devices.id == device_id).one()
+
+        project_membership = self.session.query(models.ProjectMemberships).filter(
+            models.ProjectMemberships.project_id == device.project_id,
+            models.ProjectMemberships.email == email
+        ).one()
+        project_membership_api_key_count = self.session.query(models.ProjectMembershipApiKeys).filter(
+            models.ProjectMembershipApiKeys.project_membership_id == project_membership.id,
+            models.ProjectMembershipApiKeys.secret == secret
+        ).count()
+
+        return True if project_membership_api_key_count else False
 
     def validate_version_format(self, version):
         validation_regex = re.compile(r'^SemVer+[0-9]+\.+[0-9]*$')
